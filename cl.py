21262de08da26315ef1047d52bc81a8cb3cf5a68
Kattie Meraz <merazk@carleton.edu>
2025-04-19 17:55:36 -0500

    Update cl.py
    added get_parsed_arguments

--- cl.py
+++ cl.py
@@ -12,3 +12,16 @@ import sys
 # python3 cl.py -- category "exercise" 
 # get a list of activities within a category
 
+
+def get_parsed_arguments():
+    '''parse the command line arguments
+    returns: the parsed arguments (argparse.Namespace)'''
+    parser = argparse.ArgumentParser(description="Get the most common activity for a given age group")
+    parser.add_argument("--age", type=int, help="the age group to get the most common activity for")
+    parser.add_argument("--top", type=int, help="the number of activities to get")
+    args = parser.parse_args()
+    
+    parser2 = argparse.ArgumentParser(description="Get a list of activities within a category")
+    parser2.add_argument("--category", type=str, help="the category to get the activities for")
+    args2 = parser2.parse_args()
+    return args
