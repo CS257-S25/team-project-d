28584ac6960a40a5aa8bfe873760fe09aa8d3c48
Kattie Meraz <merazk@carleton.edu>
2025-04-19 16:38:01 -0500

    Create loadData.py

---
+++ ProductionCode/loadData.py
@@ -0,0 +1,5 @@
+def load_data():
+    ''' Purpose: Load data from a file
+    Returns: None'''
+    file_name = open("dummy_data.csv", "r")
+    print("Loading data from file...") #are we allowed to print?
