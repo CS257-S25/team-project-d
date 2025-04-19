680aa8804f101909e2a00b1c5e8441ac04c8950a
Kattie Meraz <merazk@carleton.edu>
2025-04-19 16:36:58 -0500

    Create getActivtyByCategory.py

---
+++ ProductionCode/getActivtyByCategory.py
@@ -0,0 +1,5 @@
+# python3 cl.py -- category "exercise" 
+# get a list of activities within a category
+def get_category():
+    ''''return: a list of activities within the category given in cl'''
+    pass
