8493f56133f0c0c571a8f288d0994bb167455a08
Kattie0554 <merazk@carleton.edu>
2025-04-20 18:09:58 -0500

    erased some clutter, specified what this data is used for

--- ProductionCode/loadData.py
+++ ProductionCode/loadData.py
@@ -6,21 +6,9 @@ def load_data():
     ''' Purpose: Load data from a file
     Returns: None'''
     print("Loading data from file...")
-    #actually use this:"Data/teamproject22-23FINAL_updatedpls.csv"
+    #this loads the data needed for get_top_by_age.py 
     with open("Data/teamproject22-23FINAL_updatedpls.csv", "r") as file:
-        ##### usually: keep these ones but actully just edit the dataset and remove first useless row
-        
-        #lines = file.readlines()
-        #     remove the first line (header) from the file because it was just the title 
-        #cleaned_lines = lines[1:]
-        #     use DictReader to read the csv file into a dictionary (avoid closing the file early)
-        #reader =csv.DictReader(cleaned_lines)
-        #data = list(reader) #convert to list to be able to pass around the data
-
-        ##############
         reader=csv.DictReader(file)
         data = list(reader)
-        #print("Sample row:", data[2])
-        #print("CSV HEaders:", reader.fieldnames)
         return data
 
