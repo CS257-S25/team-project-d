7db52c9261841f0b3390d6151dcde3508c753b96
Claire Holmes <holmes.claire13@gmail.com>
2025-05-03 17:15:34 -0500

    add get top by age to datasourse

--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -15,7 +15,7 @@ class DataSource:
         try:
             connection = psycopg2.connect(database=config.DATABASE, user=config.USER,
             password=config.PASSWORD, host="localhost")
-        except ConnectionError as e:
+        except psycopg2.Error as e:
             print("Connection error: ", e)
             sys.exit()
         return connection
@@ -48,3 +48,28 @@ class DataSource:
             print ("Something went wrong when executing the query: ", e)
             return None
 
+    def get_top_by_age(self, age):
+        '''finds the top activity for a given age
+        param age: the age to find the top activity for'''
+        try:
+            cursor = self.connection.cursor()
+            query = "SELECT " \
+            "CASE " \
+            "WHEN SUM(Sleeping) > SUM(Hygiene) AND SUM(Sleeping) > SUM(Cleaning) AND " \
+            "SUM(Sleeping) > SUM(Work) AND SUM(Sleeping) > SUM(Eating) THEN 'Sleeping' " \
+            "WHEN SUM(Hygiene) > SUM(Sleeping) AND SUM(Hygiene) > SUM(Cleaning) AND " \
+            "SUM(Hygiene) > SUM(Work) AND SUM(Hygiene) > SUM(Eating) THEN 'Hygiene' " \
+            "WHEN SUM(Cleaning) > SUM(Sleeping) AND SUM(Cleaning) > SUM(Hygiene) AND " \
+            "SUM(Cleaning) > SUM(Work) AND SUM(Cleaning) > SUM(Eating) THEN 'Cleaning' " \
+            "WHEN SUM(Work) > SUM(Sleeping) AND SUM(Work) > SUM(Hygiene) AND SUM(Work) > " \
+            "SUM(Cleaning) AND SUM(Work) > SUM(Eating) THEN 'Work' " \
+            "WHEN SUM(Eating) > SUM(Sleeping) AND SUM(Eating) > SUM(Hygiene) AND " \
+            "SUM(Eating) > SUM(Cleaning) AND SUM(Eating) > SUM(Work) THEN 'Eating' " \
+            "END AS top_activity " \
+            "FROM time_use WHERE age=%s"
+
+            cursor.execute(query, (age,))
+            print(cursor.fetchall())
+
+        except psycopg2.Error as e:
+            print ("Something went wrong when executing the query: ", e)
