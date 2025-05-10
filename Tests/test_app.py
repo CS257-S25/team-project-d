de9d44c740b1df2b269b543f304b4370659b4a1d
Camila <mendozac@stearns.mathcs.carleton.edu>
2025-05-10 14:21:02 -0400

    mocking data

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -3,6 +3,7 @@ file: test_app.py'''
 import unittest
 import psycopg2
 from app import app
+from unittest.mock import patch
 
 from app import get_subcategories_for_category, page_not_found, python_bug
 from app import get_activities_from_sub, compare_activity_for_age, get_all_categories
@@ -15,37 +16,74 @@ class TestApp(unittest.TestCase):
         app.config['TESTING']= True
         self.app = app.test_client()
 
-    def test_route_home(self):
+    @patch("app.homepage")
+    def test_route_home(self, mock_homepage):
         '''tests that the home route returns the correct thing'''
+        mock_homepage.return_value = b"This is the homepage for the time use project! "\
+        b"1) TO GET the top activity for a certain age between 15 and 80, go to /get-top/'<'age'>' "\
+        b"For example: http://127.0.0.1:5000/get-top/23 "\
+        b"2) TO COMPARE the top activity for a certain age from 2022/2023 to 2012/2013, go to /compare/'<'age'>'/'<'activity'>' "\
+        b"For example: http://127.0.0.1:5000/compare/23/Sleeping "\
+        
+        b"To see all options, use any of the following: "\
+        b"A) TO GET a list of all category options, go to /get-all-categories "\
+        b"B) TO GET a list of subcategory options from a category, "\
+        b"go to /get-subcategories/'<'category'>' C) TO GET a list of activities from a subcategory, "\
+        b"go to /get-activities/'<'category'>'/'<'subcategory'>'"
         response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(b"This is the homepage: "\
-            b" 1) TO GET the top activity for a certain age, go to /get-top/'<'age'>'"\
-            b" 2) TO GET a list of all category options, go to /get-all-categories "\
-            b" 3) TO GET a list of subcategory options from a category, "\
-            b"go to /get-subcategories/'<'category'>' "\
-            b" 4) TO GET a list of activities from a subcategory, "\
-            b"go to /get-activities/'<'category'>'/'<'subcategory'>'", response.data )
+        self.assertEqual(b"This is the homepage for the time use project! "\
+        b"1) TO GET the top activity for a certain age between 15 and 80, go to /get-top/'<'age'>' "\
+        b"For example: http://127.0.0.1:5000/get-top/23 "\
+        b"2) TO COMPARE the top activity for a certain age from 2022/2023 to 2012/2013, go to /compare/'<'age'>'/'<'activity'>' "\
+        b"For example: http://127.0.0.1:5000/compare/23/Sleeping "\
+        
+        b"To see all options, use any of the following: "\
+        b"A) TO GET a list of all category options, go to /get-all-categories "\
+        b"B) TO GET a list of subcategory options from a category, "\
+        b"go to /get-subcategories/'<'category'>' C) TO GET a list of activities from a subcategory, "\
+        b"go to /get-activities/'<'category'>'/'<'subcategory'>'", response.data )
 
-    def test_route_top_by_age(self):
+    @patch("app.get_top_by_age")
+    def test_route_top_by_age(self, mock_get_top_by_age):
         '''tests that the route to get top by age returns the right thing, given age 23'''
+        mock_get_top_by_age.return_value = "the top activity for people age 23 is Sleeping"
         response = get_top_by_age(23)
         self.assertEqual("the top activity for people age 23 is Sleeping", response)
 
-    def test_get_all_categories(self):
+    @patch("app.get_all_categories")
+    def test_get_all_categories(self, mock_get_all_categories):
         '''tests that the route to get all categories returns the correct thing'''
+        mock_get_all_categories.return_value = "The category options are: ['Personal_Care_Activities', "\
+        "'Household_Activities', 'Caring_For_&_Helping_Household_(HH)_Members', "\
+        "'Caring_For_&_Helping_Nonhousehold_(NonHH)_Members', 'Work_&_Work-Related_Activities', "\
+        "'Education', 'Consumer_Purchases', 'Professional_&_Personal_Care_Services', "\
+        "'Household_Services', 'Government_Services_&_Civic_Obligations', 'Eating_and_Drinking', "\
+        "'Socializing_Relaxing_and_Leisure', 'Sports_Exercise_&_Recreation', 'Religious_and_Spiritual_Activities', "\
+        "'Volunteer_Activities', 'Telephone_Calls', 'Traveling']"
         response = get_all_categories()
-        self.assertEqual("The category options are: [('T01', 'Personal_Care_Activities'), ('T02', 'Household_Activities'), ('T03', 'Caring_For_&_Helping_Household_(HH)_Members'), ('T04', 'Caring_For_&_Helping_Nonhousehold_(NonHH)_Members'), ('T05', 'Work_&_Work-Related_Activities'), ('T06', 'Education'), ('T07', 'Consumer_Purchases'), ('T08', 'Professional_&_Personal_Care_Services'), ('T09', 'Household_Services'), ('T10', 'Government_Services_&_Civic_Obligations'), ('T11', 'Eating_and_Drinking'), ('T12', 'Socializing_Relaxing_and_Leisure'), ('T13', 'Sports_Exercise_&_Recreation'), ('T14', 'Religious_and_Spiritual_Activities'), ('T15', 'Volunteer_Activities'), ('T16', 'Telephone_Calls'), ('T18', 'Traveling')]", response)
+        self.assertEqual("The category options are: ['Personal_Care_Activities', "\
+        "'Household_Activities', 'Caring_For_&_Helping_Household_(HH)_Members', "\
+        "'Caring_For_&_Helping_Nonhousehold_(NonHH)_Members', 'Work_&_Work-Related_Activities', "\
+        "'Education', 'Consumer_Purchases', 'Professional_&_Personal_Care_Services', 'Household_Services', "\
+        "'Government_Services_&_Civic_Obligations', 'Eating_and_Drinking', 'Socializing_Relaxing_and_Leisure', "\
+        "'Sports_Exercise_&_Recreation', 'Religious_and_Spiritual_Activities', 'Volunteer_Activities', "\
+        "'Telephone_Calls', 'Traveling']", response)
 
-    def test_get_subcategories_for_category(self):
+    @patch("app.get_subcategories_for_category")
+    def test_get_subcategories_for_category(self, mock_get_subcategories_for_category):
         '''tests that the route to get subcategories given a category returns the right thing '''
+        mock_get_subcategories_for_category.return_value = "These are the subcategories for Personal_Care_Activities : "\
+        "['Sleeping', 'Grooming', 'Health-related_self_care', 'Personal_Activities', 'Personal_Care_Emergencies']"
         result = get_subcategories_for_category('Personal_Care_Activities')
-        self.assertEqual("These are the subcategories for Personal_Care_Activities : [('T0102', 'Grooming'), ('T0103', 'Health-related_self_care'), ('T0104', 'Personal_Activities'), ('T0105', 'Personal_Care_Emergencies')]", result)
+        self.assertEqual("These are the subcategories for Personal_Care_Activities : "\
+        "['Sleeping', 'Grooming', 'Health-related_self_care', 'Personal_Activities', 'Personal_Care_Emergencies']", result)
 
-    #
-    def test_get_activities_from_sub(self):
+    @patch("app.get_activities_from_sub")
+    def test_get_activities_from_sub(self, mock_get_activities_from_sub):
         '''tests that the route to get activities returns the correct thing '''
+        mock_get_activities_from_sub.return_value = "here are the activities for Sleeping in Personal_Care_Activities: ['Sleeping', 'Sleeplessness']"
         result = get_activities_from_sub('Personal_Care_Activities', 'Sleeping')
-        self.assertEqual("here are the activities for Sleeping in Personal_Care_Activities: not found ", result)
+        self.assertEqual("here are the activities for Sleeping in Personal_Care_Activities: ['Sleeping', 'Sleeplessness']", result)
 
     def assert_404(self, route):
         '''test to make sure error returns correct thing'''
