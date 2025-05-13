73658394f8efe5d28989b86a48a0daaedc342c30
Camila <mendozac@carleton.edu>
2025-05-12 19:29:28 -0500

    maybe

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_top_by_age(age):
     top = test.get_top_by_age(age)
     if "invalid age" in top:
         return top
-    return "the top activity for people age " + age + " is " + str(top)
+    return "the top activity for people age " + age + " is " + f"{top}"
 
 @app.route('/get-top/')
 def missing_age():
