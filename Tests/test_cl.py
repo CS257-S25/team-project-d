7406a79b6a7b8e722da64c90e4abf707a24a3d7c
Camila <mendozac@stearns.mathcs.carleton.edu>
2025-05-13 22:54:01 -0400

    compare

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -21,7 +21,7 @@ class TestCL(unittest.TestCase):
         self.mock_cursor = self.mock_conn.cursor.return_value
         self.app = app.test_client()
 
-    @patch("cl.datasource.DataSource")
+    #@patch("cl.datasource.DataSource")
     @patch("cl.get_parsed_arguments")
     def test_main_compare(self, mock_print, mock_datasource_class):
         '''tests the main function'''
