e61b3a68e2f241793a4b7fc14e7a07494871c228
Camila <mendozac@stearns.mathcs.carleton.edu>
2025-05-13 22:56:30 -0400

    compare

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -23,7 +23,7 @@ class TestCL(unittest.TestCase):
 
     #@patch("cl.datasource.DataSource")
     @patch("cl.get_parsed_arguments")
-    def test_main_compare(self, mock_print):
+    def test_main_compare(self, mock_datasource_class):
         '''tests the main function'''
         mock_instance = MagicMock()
         mock_instance.age = None
