a0d57a6f4175dd47b86a303ea34a7cc6f41dd803
Claire Holmes <holmes.claire13@gmail.com>
2025-05-18 16:01:18 -0500

    again

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -11,7 +11,7 @@ class TestDataSource(unittest.TestCase):
         self.mock_cursor = self.mock_conn.cursor.return_value
 
     @patch("ProductionCode.datasource.psycopg2.connect")
-    @patch("ProductionCode.datasource.DataSource.get_id_from_name")
+    # @patch("ProductionCode.datasource.DataSource.get_id_from_name")
     def test_get_activity_list_error(self, mock_get_id_from_name):
         '''tests the correct error message for get_activity_list when subcategory is not found'''
         # self.mock_conn.cursor.return_value.fetchall.return_value = ["testingggg????"]
