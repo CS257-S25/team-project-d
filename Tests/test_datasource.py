a5aabcba36ffe2d17ae4377dd787efc58b870b99
Claire Holmes <holmes.claire13@gmail.com>
2025-05-18 15:25:10 -0500

    not going to test connection ig

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -1,7 +1,7 @@
 '''tests for datasource.py'''
 import unittest
 from unittest.mock import MagicMock, patch
-from ProductionCode.datasource import DataSource
+from ProductionCode.datasource import datasource
 
 class TestDataSource(unittest.TestCase):
     '''class for tests for datasource.py'''
@@ -9,17 +9,12 @@ class TestDataSource(unittest.TestCase):
         #create a mock connection and cursor
         self.mock_conn = MagicMock()
         self.mock_cursor = self.mock_conn.cursor.return_value
-        self.datasource = DataSource.datasource.test_client()
-
-    def test_connect(self):
-        '''test connection in datasource'''
-        self.assertRaises(SystemExit, self.datasource.connect)
-        self.mock_conn.cursor.assert_called_once()
+        self.datasource = datasource.test_client()
 
     def test_get_activity_list_error(self):
         '''tests the correct error message for get_activity_list when subcategory is not found'''
         self.mock_conn.cursor.return_value.fetchall.return_value = ["testingggg????"]
-        not_found = DataSource.get_subcategory_list("invalid_subcategory")
+        not_found = datasource.get_subcategory_list("invalid_subcategory")
         self.assertEqual(not_found, None)
-        
+
         
