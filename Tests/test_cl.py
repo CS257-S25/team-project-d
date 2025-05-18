41b97f5793cf88514ab9cd771877f774242267d8
Camila <mendozac@stearns.mathcs.carleton.edu>
2025-05-18 17:19:25 -0400

    trying to make helper for validating

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -100,7 +100,7 @@ class TestCL(unittest.TestCase):
         except cl.InvalidCategoryError:
             self.fail("validate_category() raised InvalidCategoryError")
         
-        mock_datasource_class.get_subcategory_list.assert_called_once_with("Personal_Care_Activities")
+        mock_datasource_class.return_value.get_subcategory_list.assert_called_once_with("Personal_Care_Activities")
 
     def validating_helper_method(self, mock_datasource_class, list_type, results, activities, activities_list):
         '''helper method for the validate_category functions'''
