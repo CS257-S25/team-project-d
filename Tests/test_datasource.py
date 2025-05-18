fd195e0579000b383cf0d549c3974f8f09bb2529
Claire Holmes <holmes.claire13@gmail.com>
2025-05-18 15:15:54 -0500

    first datasource test

---
+++ Tests/test_datasource.py
@@ -0,0 +1,20 @@
+'''tests for datasource.py'''
+import unittest
+from unittest.mock import MagicMock, patch
+from ProductionCode.datasource import DataSource
+
+class TestDataSource(unittest.TestCase):
+    '''class for tests for datasource.py'''
+    def setUp(self):
+        #create a mock connection and cursor
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
+        self.datasource = DataSource()
+        self.datasource.connection = self.mock_conn
+
+
+    def test_get_activity_list_error(self):
+        '''tests the correct error message for get_activity_list when subcategory is not found'''
+        not_found = DataSource.get_subcategory_list("invalid_subcategory")
+        self.assertEqual(not_found, None)
+        
