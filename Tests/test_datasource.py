95f176ceb515823b1172ae82a26fb41e0c9766a9
Claire Holmes <holmes.claire13@gmail.com>
2025-05-18 16:04:45 -0500

    ugh

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -2,6 +2,7 @@
 import unittest
 from unittest.mock import MagicMock, patch
 from ProductionCode.datasource import DataSource
+from ProductionCode.datasource import get_activity_list
 
 class TestDataSource(unittest.TestCase):
     '''class for tests for datasource.py'''
@@ -11,13 +12,12 @@ class TestDataSource(unittest.TestCase):
         self.mock_cursor = self.mock_conn.cursor.return_value
 
     @patch("ProductionCode.datasource.psycopg2.connect")
-    # @patch("ProductionCode.datasource.DataSource.get_id_from_name")
-    def test_get_activity_list_error(self, mock_get_id_from_name):
+    def test_get_activity_list_error(self, mock_get_activity_list):
         '''tests the correct error message for get_activity_list when subcategory is not found'''
         # self.mock_conn.cursor.return_value.fetchall.return_value = ["testingggg????"]
-        mock_get_id_from_name.return_value = self.mock_conn
+        mock_get_activity_list.return_value = self.mock_conn
         self.mock_cursor.fetchall.return_value = None
-        not_found = DataSource.get_activity_list("invalid_subcategory")
+        not_found = get_activity_list("invalid_subcategory")
         self.assertEqual(not_found, None)
 
         
