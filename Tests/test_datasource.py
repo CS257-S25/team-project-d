c3c8e212df591d817388dada7840681937384f3f
Claire Holmes <holmes.claire13@gmail.com>
2025-05-18 16:10:43 -0500

    please

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -2,7 +2,6 @@
 import unittest
 from unittest.mock import MagicMock, patch
 from ProductionCode.datasource import DataSource
-from ProductionCode.datasource import get_activity_list
 
 class TestDataSource(unittest.TestCase):
     '''class for tests for datasource.py'''
@@ -12,12 +11,14 @@ class TestDataSource(unittest.TestCase):
         self.mock_cursor = self.mock_conn.cursor.return_value
 
     @patch("ProductionCode.datasource.psycopg2.connect")
-    def test_get_activity_list_error(self, mock_get_activity_list):
+    @patch("ProductionCode.datasource.get_id_from_name")
+    def test_get_activity_list_error(self, mock_get_id_from_name, mock_connect):
         '''tests the correct error message for get_activity_list when subcategory is not found'''
-        # self.mock_conn.cursor.return_value.fetchall.return_value = ["testingggg????"]
-        mock_get_activity_list.return_value = self.mock_conn
-        self.mock_cursor.fetchall.return_value = None
-        not_found = get_activity_list("invalid_subcategory")
+        mock_get_id_from_name.return_value = None
+        mock_connect.return_value = self.mock_conn
+        ds = DataSource()
+        not_found = ds.get_activity_list("invalid_subcategory")
         self.assertEqual(not_found, None)
+        self.mock_cursor.execute.assert_not_called()
 
         
