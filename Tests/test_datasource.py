bb66395610e09a2ca5694424b25244018bdca5a3
Claire Holmes <holmes.claire13@gmail.com>
2025-05-19 13:13:38 -0500

    fetchall return value

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -144,7 +144,7 @@ class TestDataSource(unittest.TestCase):
         '''tests the correct error message for get_activity_list when subcategory is not found'''
         mock_get_id_from_name.return_value = "T010101"
         mock_connect.return_value = self.mock_conn
-        mock_connect.cursor.return_value = self.mock_cursor
+        self.mock_cursor.fetchall.return_value = (559, 552)
         ds = DataSource()
         result = ds.compare_by_age(23, "Sleeping")
         print(f"compare by age result: {result}")
