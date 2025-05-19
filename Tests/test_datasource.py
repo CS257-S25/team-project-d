c0154b672cefbdc2204557d7c97c26de915749e6
Claire Holmes <holmes.claire13@gmail.com>
2025-05-19 10:32:12 -0500

    who knows

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -37,7 +37,7 @@ class TestDataSource(unittest.TestCase):
     def test_get_correct_list(self, mock_connect):
         '''tests the correct list is returned from get_correct_list'''
         mock_connect.return_value = self.mock_conn
-        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchall.side_effect = psycopg2.Error()
         ds = DataSource()
         result = ds.get_correct_list("test", "SELECT")
         self.assertEqual(result, None)
