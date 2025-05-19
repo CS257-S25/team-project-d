9d4dee3d9d1c129582bdd4a3eccad8f3451a2ba3
Claire Holmes <holmes.claire13@gmail.com>
2025-05-19 10:35:34 -0500

    i dont understand mock

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -37,6 +37,7 @@ class TestDataSource(unittest.TestCase):
     def test_get_correct_list(self, mock_connect):
         '''tests the correct list is returned from get_correct_list'''
         mock_connect.return_value = self.mock_conn
+        self.mock_cursor.return_value = self.mock_cursor
         self.mock_cursor.fetchall.side_effect = psycopg2.Error()
         ds = DataSource()
         result = ds.get_correct_list("test", "SELECT")
