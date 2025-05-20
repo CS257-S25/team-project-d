85292562f074770d47f40547e285f5d48f1dcc9d
Camila <mendozac@stearns.mathcs.carleton.edu>
2025-05-19 22:43:54 -0400

    show activity form

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -147,7 +147,6 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/show_find_activities?category=Personal_Care_Activities&subcategory=Sleeping')
         self.assertIn(b"Sleeping, Sleeplessness", response.data)
 
-
     @patch("app.helper")
     def test_helper(self, mock_helper):
         '''test that the helper function returns the right thing'''
