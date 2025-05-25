c0253c2c2fbd91c66799eaa53f9728f3c2eb92ae
kd <merazk@carleton.edu>
2025-05-25 18:17:20 -0400

    hella stuff

--- static/get_top_chart.js
+++ static/get_top_chart.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const ctx = document.getElementById('topActivityChart').getContext('2d');
 
-    const labels = JSON.parse(document.getElementById('chart-data').dataset.labels);
-    const times = JSON.parse(document.getElementById('chart-data').dataset.times);
+    const labels = JSON.parse(document.getElementById('get_top-data').dataset.labels);
+    const times = JSON.parse(document.getElementById('get_top-data').dataset.times);
 
     const topActivityChart = new Chart(ctx, {
         type: 'bar',
-        data: {
+        data: { 
             labels: labels,
             datasets: [{
-                label: 'Average Hours Spent',
+                label: 'Average Hours Spent Doing This Activity',
                 data: times,
-                backgroundColor: 'rgba(50, 7, 63, 0.7)',
+                backgroundColor: ['rgba(55,20,88, 0.7)', 'rgba(146, 183, 150,0.7)', 'rgba(148, 199, 188, 0.7)'],
                 borderWidth: 1
             }]
         },
@@ -21,13 +21,17 @@ document.addEventListener('DOMContentLoaded', function () {
                     beginAtZero: true,
                     title: {
                         display: true,
-                        text: "Average Hours Spent Doing This Activity"
+                        text: "Average Hours Spent"
                     }
                 },
                 x: {
                     title: {
                         display: true,
                         text: "Activity"
+                    },
+                    ticks: {
+                        maxRotation: 0,
+                        minRotation: 0
                     }
                 }
             }
