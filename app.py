15d64c8bbf6d75763fe640cc7023624a447170f1
holmesclaire <holmes.claire13@gmail.com>
2025-06-02 13:10:20 -0400

    app.run(host) for usability testing

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def python_bug(e):
                             message = "ERROR 500: INTERNAL SERVER ERROR")
 
 if __name__ == '__main__':
-    app.run(port=5138)
+    app.run(host='0.0.0.0', debug=False, port=5138)
