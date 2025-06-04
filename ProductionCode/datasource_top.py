a3b1d9083c9ff2ceb735074d11bbeca5a6257a67
Claire Holmes <holmes.claire13@gmail.com>
2025-06-04 14:39:58 -0500

    try to fix duplicate connection functions

--- ProductionCode/datasource_top.py
+++ ProductionCode/datasource_top.py
@@ -5,14 +5,14 @@ from ProductionCode import psql_config as config
 from ProductionCode.datasource_activities import DataSource as activities
 
 class DataSource:
-    '''Class to connect to database and create sql table'''
+    '''Class to connect to database and create sql table for Get Top Activity By Age'''
     def __init__(self):
-        '''Constructor that initiates connection to database'''
+        '''Constructor that initiates connection to SQL database'''
         self.connection = self.connect()
 
     def connect(self):
-        '''Initiates connection to database using information in the psql_config.py file.
-        Returns the connection object.'''
+        '''Initiates connection to database using information in the gitignore
+        psql_config.py file. Returns the connection object.'''
         try:
             connection = psycopg2.connect(database=config.DATABASE, user=config.USER,
             password=config.PASSWORD, host="localhost")
